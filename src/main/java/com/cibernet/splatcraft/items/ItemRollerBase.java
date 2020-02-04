f48ca6ee66d8eeea761765ec93e03fed14bc690e
Cibernet <Cibernet83@users.noreply.github.com>
2020-02-04 15:58:18 -0500

    Started working on the Splat Roller's code

---
+++ src/main/java/com/cibernet/splatcraft/items/ItemRollerBase.java
@@ -0,0 +1,128 @@
+package com.cibernet.splatcraft.items;
+
+import javax.annotation.Nullable;
+
+import com.cibernet.splatcraft.utils.SplatCraftUtils;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.entity.EntityLiving;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.ai.attributes.AttributeModifier;
+import net.minecraft.entity.item.EntityItem;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.projectile.EntityArrow;
+import net.minecraft.init.Enchantments;
+import net.minecraft.init.Items;
+import net.minecraft.init.SoundEvents;
+import net.minecraft.item.*;
+import net.minecraft.stats.StatList;
+import net.minecraft.util.*;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+public class ItemRollerBase extends ItemWeaponBase
+{
+    private static final AttributeModifier SPRINTING_SPEED_BOOST = (new AttributeModifier( "Sprinting speed boost", 2D, 2)).setSaved(false);
+
+    public ItemRollerBase(String unlocName, String registryName)
+    {
+        super(unlocName, registryName);
+        this.maxStackSize = 1;
+        this.setMaxDamage(384);
+        this.setCreativeTab(CreativeTabs.COMBAT);
+        this.addPropertyOverride(new ResourceLocation("unfolded"), new IItemPropertyGetter()
+        {
+            @SideOnly(Side.CLIENT)
+            public float apply(ItemStack stack, @Nullable World worldIn, @Nullable EntityLivingBase entityIn)
+            {
+                return entityIn != null && entityIn.isHandActive() && entityIn.getActiveItemStack() == stack ? 1.0F : 0.0F;
+            }
+        });
+    }
+
+    private ItemStack findAmmo(EntityPlayer player)
+    {
+        if (this.isArrow(player.getHeldItem(EnumHand.OFF_HAND)))
+        {
+            return player.getHeldItem(EnumHand.OFF_HAND);
+        }
+        else if (this.isArrow(player.getHeldItem(EnumHand.MAIN_HAND)))
+        {
+            return player.getHeldItem(EnumHand.MAIN_HAND);
+        }
+        else
+        {
+            for (int i = 0; i < player.inventory.getSizeInventory(); ++i)
+            {
+                ItemStack itemstack = player.inventory.getStackInSlot(i);
+
+                if (this.isArrow(itemstack))
+                {
+                    return itemstack;
+                }
+            }
+
+            return ItemStack.EMPTY;
+        }
+    }
+
+    protected boolean isArrow(ItemStack stack)
+    {
+        return stack.getItem() instanceof ItemArrow;
+    }
+
+    /**
+     * Called when the player stops using an Item (stops holding the right mouse button).
+     */
+    public void onPlayerStoppedUsing(ItemStack stack, World worldIn, EntityLivingBase entityLiving, int timeLeft)
+    {
+        if(entityLiving.getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).hasModifier(SPRINTING_SPEED_BOOST))
+            entityLiving.getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).removeModifier(SPRINTING_SPEED_BOOST);
+    }
+
+    /**
+     * Gets the velocity of the arrow entity from the bow's charge
+     */
+    public static float getArrowVelocity(int charge)
+    {
+        float f = (float)charge / 20.0F;
+        f = (f * f + f * 2.0F) / 3.0F;
+
+        if (f > 1.0F)
+        {
+            f = 1.0F;
+        }
+
+        return f;
+    }
+
+    /**
+     * How long it takes to use or consume an item
+     */
+    public int getMaxItemUseDuration(ItemStack stack)
+    {
+        return 72000;
+    }
+
+    /**
+     * Called when the equipped item is right clicked.
+     */
+    public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand handIn)
+    {
+        ItemStack stack = playerIn.getHeldItem(handIn);
+
+        SplatCraftUtils.inkBlock(worldIn, new BlockPos(playerIn.posX+1, playerIn.posY-1, playerIn.posZ), getInkColor(stack));
+
+        if(!playerIn.getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).hasModifier(SPRINTING_SPEED_BOOST))
+            playerIn.getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).applyModifier(SPRINTING_SPEED_BOOST);
+        else playerIn.getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).removeModifier(SPRINTING_SPEED_BOOST);
+        playerIn.getItemInUseCount();
+
+        playerIn.setActiveHand(handIn);
+        return new ActionResult(EnumActionResult.SUCCESS, stack);
+
+    }
+}
