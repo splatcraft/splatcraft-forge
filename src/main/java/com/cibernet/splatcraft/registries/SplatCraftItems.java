ce6069598d391336cc3df440931a9afc78c48325
Cibernet <cyberneticGuardian@gmail.com>
2020-02-15 19:45:22 -0500

    Added the Inkbrush to the game

--- src/main/java/com/cibernet/splatcraft/registries/SplatCraftItems.java
+++ src/main/java/com/cibernet/splatcraft/registries/SplatCraftItems.java
@@ -22,7 +22,8 @@ public class SplatCraftItems
     public static final Item splatRoller = new ItemRollerBase("splatRoller", "splat_roller", -3d, 0.4f);
     public static final Item splatCharger = new ItemShooterBase("splatCharger", "splat_charger", 0.5f, 5f, 2f, 50, false);
     public static final Item splattershotJr = new ItemShooterBase("splattershotJr", "splattershot_jr", 1f, 0.3f, 3.5f, 4);
-
+    public static final Item inkbrush = new ItemRollerBase("inkbrush", "inkbrush", 8D, 0.2f);
+    
     @SubscribeEvent
     public static void registerItems(RegistryEvent.Register<Item> event)
     {
@@ -32,6 +33,7 @@ public class SplatCraftItems
         registerItem(registry, splatRoller);
         registerItem(registry, splatCharger);
         registerItem(registry, splattershotJr);
+        registerItem(registry, inkbrush);
     
         registerItemBlocks(registry);
     }
