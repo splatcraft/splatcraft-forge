3f862adb755f50108df2a71fbcb6ec9fa4120faf
Cibernet <Cibernet83@users.noreply.github.com>
2020-02-17 11:49:08 -0500

    Inked blocks now retain their saved block's blast resistance

--- src/main/java/com/cibernet/splatcraft/blocks/BlockInked.java
+++ src/main/java/com/cibernet/splatcraft/blocks/BlockInked.java
@@ -6,6 +6,7 @@ import net.minecraft.block.Block;
 import net.minecraft.block.material.MapColor;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
@@ -14,6 +15,7 @@ import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.RayTraceResult;
+import net.minecraft.world.Explosion;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 
@@ -72,7 +74,25 @@ public class BlockInked extends BlockInkColor
 		TileEntityInkedBlock te = (TileEntityInkedBlock) worldIn.getTileEntity(pos);
 		return te.getSavedState().getBlock().getBlockHardness(te.getSavedState(), worldIn, pos);
 	}
-	
+
+	@Override
+	public float getExplosionResistance(World worldIn, BlockPos pos, @Nullable Entity exploder, Explosion explosion)
+	{
+
+		if(!(worldIn.getTileEntity(pos) instanceof TileEntityInkedBlock))
+			return super.getExplosionResistance(worldIn, pos, exploder, explosion);
+		TileEntityInkedBlock te = (TileEntityInkedBlock) worldIn.getTileEntity(pos);
+		return te.getSavedState().getBlock().getExplosionResistance(worldIn, pos, exploder, explosion);
+	}
+
+	@Override
+	public void harvestBlock(World worldIn, EntityPlayer player, BlockPos pos, IBlockState state, @Nullable TileEntity te, ItemStack stack)
+	{
+		if(!(worldIn.getTileEntity(pos) instanceof TileEntityInkedBlock))
+			super.harvestBlock(worldIn, player, pos, state, te, stack);
+		else ((TileEntityInkedBlock)te).getSavedState().getBlock().harvestBlock(worldIn, player, pos, state, te, stack);
+	}
+
 	@Override
 	public void updateTick(World worldIn, BlockPos pos, IBlockState state, Random rand)
 	{
