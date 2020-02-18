60b18761eec1b8b90d87ac07a001446eeb3a2005
Cibernet <Cibernet83@users.noreply.github.com>
2020-02-18 14:47:44 -0500

    Added some blocks

--- src/main/java/com/cibernet/splatcraft/registries/SplatCraftBlocks.java
+++ src/main/java/com/cibernet/splatcraft/registries/SplatCraftBlocks.java
@@ -3,7 +3,9 @@ package com.cibernet.splatcraft.registries;
 import com.cibernet.splatcraft.blocks.BlockInkColor;
 import com.cibernet.splatcraft.blocks.BlockInked;
 import com.cibernet.splatcraft.blocks.BlockInkwell;
+import com.cibernet.splatcraft.utils.TabSplatCraft;
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockGlass;
 import net.minecraft.block.material.Material;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -13,7 +15,12 @@ public class SplatCraftBlocks
 {
 
     public static Block inkedBlock = new BlockInked();
+
+    public static Block emptyInkwell = new BlockGlass(Material.GLASS, true).setHardness(0.5f).setCreativeTab(TabSplatCraft.main).setUnlocalizedName("inkwellEmpty").setRegistryName("inkwell_empty");
     public static Block inkwell = new BlockInkwell();
+
+    public static Block sunkenCrate = new Block(Material.WOOD).setCreativeTab(TabSplatCraft.main).setUnlocalizedName("sunkenCrate").setRegistryName("sunken_crate"); //TODO
+    public static Block oreSardinium = new Block(Material.ROCK).setCreativeTab(TabSplatCraft.main).setUnlocalizedName("oreSardinium").setRegistryName("sardinium_ore");
     
     @SubscribeEvent
     public static void registerBlocks(RegistryEvent.Register<Block> event)
@@ -21,8 +28,11 @@ public class SplatCraftBlocks
         IForgeRegistry<Block> registry = event.getRegistry();
         
         registerBlock(registry, inkedBlock);
+        registerBlock(registry, emptyInkwell, true);
         registerBlock(registry, inkwell);
-        
+        registerBlock(registry, sunkenCrate, true);
+        registerBlock(registry, oreSardinium, true);
+
     }
 
 
