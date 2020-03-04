24f125edc85d08a98269cfa1af69951bfcb779c3
Cibernet <Cibernet83@users.noreply.github.com>
2020-03-04 08:22:42 -0500

    moved attribute modifiers to the client event

--- src/main/java/com/cibernet/splatcraft/handlers/ClientEventHandler.java
+++ src/main/java/com/cibernet/splatcraft/handlers/ClientEventHandler.java
@@ -1,32 +1,89 @@
 package com.cibernet.splatcraft.handlers;
 
 import com.cibernet.splatcraft.entities.renderers.RenderInklingSquid;
-import com.cibernet.splatcraft.network.SplatCraftChannelHandler;
-import com.cibernet.splatcraft.network.SplatCraftPacket;
-import com.cibernet.splatcraft.network.tutorial.PacketPlayerSetTransformed;
-import com.cibernet.splatcraft.network.tutorial.SplatCraftPacketHandler;
+import com.cibernet.splatcraft.items.ItemWeaponBase;
+import com.cibernet.splatcraft.network.PacketPlayerSetTransformed;
+import com.cibernet.splatcraft.network.PacketWeaponLeftClick;
+import com.cibernet.splatcraft.network.SplatCraftPacketHandler;
 import com.cibernet.splatcraft.utils.SplatCraftPlayerData;
 import com.cibernet.splatcraft.utils.SplatCraftUtils;
 import net.minecraft.client.Minecraft;
 import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.ai.attributes.AttributeModifier;
+import net.minecraft.entity.ai.attributes.IAttributeInstance;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.client.event.FOVUpdateEvent;
 import net.minecraftforge.client.event.RenderPlayerEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.InputEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent;
 
 public class ClientEventHandler
 {
 	public static ClientEventHandler instance = new ClientEventHandler();
+
+	public static final AttributeModifier IN_USE_SPEED_BOOST = (new AttributeModifier( "Weapon use speed boost", 4D, 2)).setSaved(false);
+	private static final AttributeModifier SQUID_LAND_SPEED = (new AttributeModifier( "Squid in land speed boost", -0.4D, 2)).setSaved(false);
+	private static final AttributeModifier SQUID_SWIM_SPEED = (new AttributeModifier( "Squid swim speed boost", 1.25D, 2)).setSaved(false);
+
+
+	@SubscribeEvent
+	public void cllientTick(TickEvent.ClientTickEvent event)
+	{
+		EntityPlayer player = Minecraft.getMinecraft().player;
+
+		if(player == null)
+			return;
+
+		IAttributeInstance attributeInstance = player.getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED);
+
+		if(attributeInstance.hasModifier(SQUID_LAND_SPEED))
+			attributeInstance.removeModifier(SQUID_LAND_SPEED);
+		if(attributeInstance.hasModifier(SQUID_SWIM_SPEED))
+			attributeInstance.removeModifier(SQUID_SWIM_SPEED);
+		if(attributeInstance.hasModifier(IN_USE_SPEED_BOOST))
+			attributeInstance.removeModifier(IN_USE_SPEED_BOOST);
+
+		AttributeModifier weaponMod = getWeaponMod(attributeInstance);
+		if(weaponMod != null)
+			attributeInstance.removeModifier(weaponMod);
+
+		if(SplatCraftPlayerData.getIsSquid(player))
+		{
+			if(SplatCraftUtils.canSquidHide(player.world, player))
+			{
+				if(!attributeInstance.hasModifier(SQUID_SWIM_SPEED))
+					attributeInstance.applyModifier(SQUID_SWIM_SPEED);
+			}
+			else if(!attributeInstance.hasModifier(SQUID_LAND_SPEED))
+				attributeInstance.applyModifier(SQUID_LAND_SPEED);
+
+		}
+
+		ItemStack weapon = player.getActiveItemStack();
+		if(weapon.getItem() instanceof ItemWeaponBase)
+		{
+			AttributeModifier speedMod = ((ItemWeaponBase) weapon.getItem()).getSpeedModifier();
+
+			if(player.getItemInUseCount() > 0)
+			{
+				if(!attributeInstance.hasModifier(IN_USE_SPEED_BOOST))
+					attributeInstance.applyModifier(IN_USE_SPEED_BOOST);
+				if(speedMod != null && !attributeInstance.hasModifier(speedMod))
+					attributeInstance.applyModifier(speedMod);
+			}
+		}
+	}
 
 	@SubscribeEvent
 	public void updateFOV(FOVUpdateEvent event)
 	{
 		float fov = event.getNewfov();
 
-		if(event.getEntity().getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).hasModifier(CommonEventHandler.IN_USE_SPEED_BOOST))
+		if(event.getEntity().getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).hasModifier(IN_USE_SPEED_BOOST))
 			fov -= 2f;
 
 		event.setNewfov(fov);
@@ -53,7 +110,7 @@ public class ClientEventHandler
 	@SubscribeEvent
 	public void onLeftClick(PlayerInteractEvent.LeftClickEmpty event)
 	{
-		SplatCraftChannelHandler.sendToServer(SplatCraftPacket.makePacket(SplatCraftPacket.Type.WEAPON_LEFT_CLICK, event.getHand()));
+		SplatCraftPacketHandler.instance.sendToServer(new PacketWeaponLeftClick(event.getEntityPlayer().getUniqueID()));
 	}
 
 
@@ -66,6 +123,18 @@ public class ClientEventHandler
 			boolean isSquid = SplatCraftPlayerData.getIsSquid(player);
 			SplatCraftPacketHandler.instance.sendToServer(new PacketPlayerSetTransformed(player.getUniqueID(), !isSquid));
 		}
+	}
+
+	private AttributeModifier getWeaponMod(IAttributeInstance instance)
+	{
+		for(ItemWeaponBase item : ItemWeaponBase.weapons)
+		{
+			if(item.getSpeedModifier() == null)
+				continue;
+			if(instance.hasModifier(item.getSpeedModifier()))
+				return item.getSpeedModifier();
+		}
+		return null;
 	}
 	
 }
--- src/main/java/com/cibernet/splatcraft/handlers/CommonEventHandler.java
+++ src/main/java/com/cibernet/splatcraft/handlers/CommonEventHandler.java
@@ -1,48 +1,28 @@
 package com.cibernet.splatcraft.handlers;
 
 import com.cibernet.splatcraft.SplatCraft;
-import com.cibernet.splatcraft.entities.renderers.RenderInklingSquid;
 import com.cibernet.splatcraft.items.ItemWeaponBase;
-import com.cibernet.splatcraft.network.PacketPlayerData;
-import com.cibernet.splatcraft.network.SplatCraftChannelHandler;
-import com.cibernet.splatcraft.network.SplatCraftPacket;
-import com.cibernet.splatcraft.network.tutorial.PacketPlayerSetColor;
-import com.cibernet.splatcraft.network.tutorial.SplatCraftPacketHandler;
+import com.cibernet.splatcraft.network.PacketPlayerSetColor;
+import com.cibernet.splatcraft.network.SplatCraftPacketHandler;
 import com.cibernet.splatcraft.registries.SplatCraftBlocks;
-import com.cibernet.splatcraft.registries.SplatCraftItems;
 import com.cibernet.splatcraft.tileentities.TileEntityColor;
 import com.cibernet.splatcraft.utils.SplatCraftPlayerData;
 import com.cibernet.splatcraft.utils.SplatCraftUtils;
-import net.minecraft.block.BlockSoulSand;
-import net.minecraft.block.BlockWeb;
-import net.minecraft.client.Minecraft;
-import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.entity.MoverType;
-import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.ai.attributes.AttributeModifier;
 import net.minecraft.entity.ai.attributes.IAttributeInstance;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.EnumFacing;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.*;
 import net.minecraft.world.storage.loot.*;
 import net.minecraft.world.storage.loot.conditions.LootCondition;
-import net.minecraftforge.client.event.RenderPlayerEvent;
 import net.minecraftforge.event.LootTableLoadEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
-import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
 
 public class CommonEventHandler
 {
-
-	public static final AttributeModifier IN_USE_SPEED_BOOST = (new AttributeModifier( "Weapon use speed boost", 4D, 2)).setSaved(false);
-
-	private static final AttributeModifier SQUID_LAND_SPEED = (new AttributeModifier( "Squid in land speed boost", -0.4D, 2)).setSaved(false);
-	private static final AttributeModifier SQUID_SWIM_SPEED = (new AttributeModifier( "Squid swim speed boost", 1.25D, 2)).setSaved(false);
 
 	public static final CommonEventHandler instance = new CommonEventHandler();
 	
@@ -50,32 +30,12 @@ public class CommonEventHandler
 	public void onTick(TickEvent.PlayerTickEvent event)
 	{
 		EntityPlayer player = event.player;
-		IAttributeInstance attributeInstance = player.getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED);
 		BlockPos pos = new BlockPos(player.posX, player.posY, player.posZ);
 
-
-		if(attributeInstance.hasModifier(SQUID_LAND_SPEED))
-			attributeInstance.removeModifier(SQUID_LAND_SPEED);
-		if(attributeInstance.hasModifier(SQUID_SWIM_SPEED))
-			attributeInstance.removeModifier(SQUID_SWIM_SPEED);
-		if(attributeInstance.hasModifier(IN_USE_SPEED_BOOST))
-			attributeInstance.removeModifier(IN_USE_SPEED_BOOST);
-
-		AttributeModifier weaponMod = getWeaponMod(attributeInstance);
-		if(weaponMod != null)
-			attributeInstance.removeModifier(weaponMod);
 
 		if(SplatCraftPlayerData.getIsSquid(player))
 		{
 			SplatCraftUtils.setEntitySize(player, 0.6f, 0.6f);
-
-			if(SplatCraftUtils.canSquidHide(player.world, player))
-			{
-				if(!attributeInstance.hasModifier(SQUID_SWIM_SPEED))
-					attributeInstance.applyModifier(SQUID_SWIM_SPEED);
-			}
-			else if(!attributeInstance.hasModifier(SQUID_LAND_SPEED))
-				attributeInstance.applyModifier(SQUID_LAND_SPEED);
 
 			if(player.world.getBlockState(pos.down()).getBlock().equals(SplatCraftBlocks.inkwell))
 				if(player.world.getTileEntity(pos.down()) instanceof TileEntityColor)
@@ -98,13 +58,7 @@ public class CommonEventHandler
 			//	attributeInstance.removeModifier(item.getSpeedModifier());
 
 			if(player.getItemInUseCount() > 0)
-			{
 				item.onItemTickUse(player.world, player, weapon, player.getItemInUseCount());
-				if(!attributeInstance.hasModifier(IN_USE_SPEED_BOOST))
-					attributeInstance.applyModifier(IN_USE_SPEED_BOOST);
-				if(item.getSpeedModifier() != null && !attributeInstance.hasModifier(item.getSpeedModifier()))
-					attributeInstance.applyModifier(item.getSpeedModifier());
-			}
 
 		}
 
