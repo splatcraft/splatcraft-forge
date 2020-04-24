a997f26d588c33089c3619154f0e0aa582e6da81
Cibernet <cyberneticGuardian@gmail.com>
2020-04-23 19:01:19 -0500

    version number up!

--- src/main/java/com/cibernet/splatcraft/SplatCraft.java
+++ src/main/java/com/cibernet/splatcraft/SplatCraft.java
@@ -21,7 +21,7 @@ public class SplatCraft
 {
 	public static final String MODID = "splatcraft";
 	public static final String NAME = "Splatcraft";
-	public static final String VERSION = "1.1.0";
+	public static final String VERSION = "2.0.0";
 	public static final String SHORT = "SC";
 
 	//TODO config
