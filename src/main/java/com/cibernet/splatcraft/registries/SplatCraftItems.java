7a42d3412380ac5f8b9835767c34901eebf0d682
Cibernet <cyberneticGuardian@gmail.com>
2020-04-23 19:01:10 -0500

    Gave the Splat Dualies a model

--- src/main/java/com/cibernet/splatcraft/registries/SplatCraftItems.java
+++ src/main/java/com/cibernet/splatcraft/registries/SplatCraftItems.java
@@ -4,11 +4,8 @@ import com.cibernet.splatcraft.entities.models.ModelInkTank;
 import com.cibernet.splatcraft.items.*;
 import com.cibernet.splatcraft.utils.TabSplatCraft;
 import net.minecraft.block.Block;
-import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemArmor;
 import net.minecraft.item.ItemBlock;
-import net.minecraftforge.common.util.EnumHelper;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.relauncher.Side;
@@ -38,7 +35,7 @@ public class SplatCraftItems
     public static final Item octobrush = new ItemRollerBase("octobrush", "octobrush", -0.1D, 0.5f, 8f, 0.95f, 3.2f, 1.2d, 2, 4, 0.18f, true);
     public static final Item eLiter4K = new ItemChargerBase("eLiter4K", "e_liter_4k", 0.85f, 12, 35, 40, 36f, 2.25f, 25f, 0.15);
     public static final Item blaster = new ItemBlasterBase("blaster", "blaster", 3f, 1.1f, 5f, 3, 18, 25, 10f, 10);
-    public static final Item splatDualie = new ItemDualieBase("splatDualies", "splat_dualies", 1f, 0.5f, 10, 10, 6, 0.75f, 1, 0.7f, 9);
+    public static final Item splatDualie = new ItemDualieBase("splatDualies", "splat_dualies", 1f, 0.5f, 10, 8, 6, 0.75f, 1, 0.7f, 9);
     
     public static final ItemInkTank inkTank = new ItemInkTank("inkTank", "ink_tank", 100);
     
@@ -84,7 +81,7 @@ public class SplatCraftItems
         
         registerItem(registry, inkDisruptor);
         registerItem(registry, turfScanner);
-
+        
         registerItemBlocks(registry);
     }
 
