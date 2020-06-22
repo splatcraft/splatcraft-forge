7cb4ea1982ee5c1564bbe8d29b462e91a9e7d7e8
Cibernet <cyberneticGuardian@gmail.com>
2020-06-21 21:20:43 -0500

    added multi-layer mode for the turfwar command

--- src/main/java/com/cibernet/splatcraft/commands/CommandTurfWar.java
+++ src/main/java/com/cibernet/splatcraft/commands/CommandTurfWar.java
@@ -55,87 +55,17 @@ public class CommandTurfWar extends CommandBase
 		BlockPos blockpos = parseBlockPos(sender, args, 0, false);
 		BlockPos blockpos1 = parseBlockPos(sender, args, 3, false);
 		World world = sender.getEntityWorld();
-		ItemRemote.RemoteResult result = ((ItemTurfScanner)SplatCraftItems.turfScanner).onRemoteUse(world, blockpos, blockpos1, null, -1, 0);
+		
+		boolean multiLayered = false;
+		
+		if(args.length > 6)
+			multiLayered = args[6].equals("true");
+		
+		ItemRemote.RemoteResult result = ((ItemTurfScanner)SplatCraftItems.turfScanner).onRemoteUse(world, blockpos, blockpos1, null, -1, multiLayered ? 1 : 0);
 		
 		if(!result.wasSuccessful())
 			throw new CommandException(result.getOutput().getUnformattedComponentText());
 		
-		/*
-		BlockPos blockpos2 = new BlockPos(Math.min(blockpos.getX(), blockpos1.getX()), Math.min(blockpos.getY(), Math.min(blockpos1.getY(), blockpos.getY())), Math.min(blockpos.getZ(), blockpos1.getZ()));
-		BlockPos blockpos3 = new BlockPos(Math.max(blockpos.getX(), blockpos1.getX()), Math.max(blockpos.getY(), Math.max(blockpos1.getY(), blockpos.getY())), Math.max(blockpos.getZ(), blockpos1.getZ()));
-		
-		if (!(blockpos2.getY() >= 0 && blockpos3.getY() < 256))
-			throw new CommandException("commands.turfWar.outOfWorld", new Object[0]);
-		
-		
-		for(int j = blockpos2.getZ(); j <= blockpos3.getZ(); j += 16)
-		{
-			for(int k = blockpos2.getX(); k <= blockpos3.getX(); k += 16)
-			{
-				if(!world.isBlockLoaded(new BlockPos(k, blockpos3.getY() - blockpos2.getY(), j)))
-				{
-					throw new CommandException("commands.turfWar.outOfWorld", new Object[0]);
-				}
-			}
-		}
-		
-		
-		TreeMap<Integer, Integer> scores = new TreeMap<>();
-		int blockTotal = 0;
-		
-		for(int x = blockpos2.getX(); x <= blockpos3.getX(); x++)
-			for(int z = blockpos2.getZ(); z <= blockpos3.getZ(); z++)
-			{
-				int y = getTopSolidOrLiquidBlock(new BlockPos(x,1, z), world, Math.min(blockpos3.getY()+2, 255)).down().getY();
-				
-				if(y < blockpos3.getY() || y < blockpos2.getY())
-					continue;
-					
-				BlockPos checkPos = new BlockPos(x,y,z);
-				IBlockState checkState = world.getBlockState(checkPos);
-				
-				if(!SplatCraftUtils.canInk(world, checkPos))
-					continue;
-				
-				if(!checkState.getMaterial().blocksMovement() || checkState.getMaterial().isLiquid() || !SplatCraftUtils.canInk(world, checkPos))
-					continue;
-				
-				blockTotal++;
-				
-				if(world.getTileEntity(checkPos) instanceof TileEntityColor && world.getBlockState(checkPos).getBlock() instanceof IInked)
-				{
-					TileEntityColor te = (TileEntityColor) world.getTileEntity(checkPos);
-					IInked block = (IInked) world.getBlockState(checkPos).getBlock();
-					int color = te.getColor();
-					
-					if(block.countsTowardsScore())
-					{
-						if(scores.containsKey(color))
-							scores.replace(color, scores.get(color) + 1);
-						else scores.put(color, 1);
-						
-					}
-					
-					
-				}
-			}
-		
-		Integer colors[] = new Integer[scores.size()];
-		Float colorScores[] = new Float[scores.size()];
-		
-		int i = 0;
-		for(Map.Entry<Integer, Integer> entry : scores.entrySet())
-		{
-			//world.getMinecraftServer().getPlayerList().sendMessage(new TextComponentTranslation("commands.turfWar.score", SplatCraftUtils.getColorName(entry.getKey()), String.format("%.1f",(entry.getValue()/(float)blockTotal)*100)));
-			colors[i] = entry.getKey();
-			colorScores[i] = (entry.getValue()/(float)blockTotal)*100;
-			i++;
-		}
-		
-		if(scores.isEmpty())
-			throw new CommandException("commands.turfWar.noInk", new Object[0]);
-		else SplatCraftPacketHandler.instance.sendToDimension(new PacketSendColorScores(colors, colorScores), sender.getEntityWorld().provider.getDimension());
-		*/
 	}
 	
 	@Override
