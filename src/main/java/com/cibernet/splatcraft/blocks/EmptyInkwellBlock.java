e3dbd2e1b088a71a0d7730e1375770988e2a4174
Cibernet <cyberneticGuardian@gmail.com>
2020-07-31 16:38:12 -0500

    Made Inkwells WaterLoggable

--- src/main/java/com/cibernet/splatcraft/blocks/EmptyInkwellBlock.java
+++ src/main/java/com/cibernet/splatcraft/blocks/EmptyInkwellBlock.java
@@ -1,13 +1,24 @@
 package com.cibernet.splatcraft.blocks;
 
 import net.minecraft.block.AbstractGlassBlock;
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.GlassBlock;
+import net.minecraft.fluid.FluidState;
+import net.minecraft.fluid.Fluids;
+import net.minecraft.item.BlockItemUseContext;
+import net.minecraft.state.BooleanProperty;
+import net.minecraft.state.StateContainer;
+import net.minecraft.state.properties.BlockStateProperties;
+import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.shapes.ISelectionContext;
 import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.util.math.shapes.VoxelShapes;
 import net.minecraft.world.IBlockReader;
+import net.minecraft.world.IWorld;
+
+import javax.annotation.Nullable;
 
 public class EmptyInkwellBlock extends AbstractGlassBlock
 {
@@ -15,11 +26,13 @@ public class EmptyInkwellBlock extends AbstractGlassBlock
 			makeCuboidShape(0, 0, 0, 16, 12, 16),
 			makeCuboidShape(1, 12, 1, 14/16f, 13, 14),
 			makeCuboidShape(0, 13, 0, 16, 16, 16));
+	public static final BooleanProperty WATERLOGGED = BlockStateProperties.WATERLOGGED;
 	
 	
 	public EmptyInkwellBlock(Properties properties)
 	{
 		super(properties.notSolid());
+		this.setDefaultState(this.stateContainer.getBaseState().with(WATERLOGGED, Boolean.valueOf(false)));
 	}
 	
 	@Override
@@ -33,4 +46,30 @@ public class EmptyInkwellBlock extends AbstractGlassBlock
 	{
 		return SHAPE;
 	}
+	
+	
+	@Nullable
+	@Override
+	public BlockState getStateForPlacement(BlockItemUseContext context)
+	{
+		
+		return getDefaultState().with(WATERLOGGED, Boolean.valueOf(context.getWorld().getFluidState(context.getPos()).getFluid() == Fluids.WATER));
+	}
+	
+	protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
+		builder.add(WATERLOGGED);
+	}
+	
+	public FluidState getFluidState(BlockState state) {
+		return state.get(WATERLOGGED) ? Fluids.WATER.getStillFluidState(false) : super.getFluidState(state);
+	}
+	
+	public BlockState updatePostPlacement(BlockState stateIn, Direction facing, BlockState facingState, IWorld worldIn, BlockPos currentPos, BlockPos facingPos) {
+		if (stateIn.get(WATERLOGGED)) {
+			worldIn.getPendingFluidTicks().scheduleTick(currentPos, Fluids.WATER, Fluids.WATER.getTickRate(worldIn));
+		}
+		
+		return super.updatePostPlacement(stateIn, facing, facingState, worldIn, currentPos, facingPos);
+	}
+	
 }
--- src/main/java/com/cibernet/splatcraft/blocks/InkwellBlock.java
+++ src/main/java/com/cibernet/splatcraft/blocks/InkwellBlock.java
@@ -9,14 +9,22 @@ import com.cibernet.splatcraft.tileentities.InkColorTileEntity;
 import com.cibernet.splatcraft.util.ColorUtils;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.IWaterLoggable;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.material.MaterialColor;
 import net.minecraft.block.material.PushReaction;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.fluid.FluidState;
+import net.minecraft.fluid.Fluids;
+import net.minecraft.item.BlockItemUseContext;
 import net.minecraft.item.ItemStack;
+import net.minecraft.state.BooleanProperty;
+import net.minecraft.state.StateContainer;
+import net.minecraft.state.properties.BlockStateProperties;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ActionResultType;
+import net.minecraft.util.Direction;
 import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.BlockRayTraceResult;
@@ -24,11 +32,12 @@ import net.minecraft.util.math.shapes.ISelectionContext;
 import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.util.math.shapes.VoxelShapes;
 import net.minecraft.world.IBlockReader;
+import net.minecraft.world.IWorld;
 import net.minecraft.world.World;
 
 import javax.annotation.Nullable;
 
-public class InkwellBlock extends Block implements IColoredBlock
+public class InkwellBlock extends Block implements IColoredBlock, IWaterLoggable
 {
 	private static final VoxelShape SHAPE = VoxelShapes.or(
 			makeCuboidShape(0, 0, 0, 16, 12, 16),
@@ -36,11 +45,38 @@ public class InkwellBlock extends Block implements IColoredBlock
 			makeCuboidShape(0, 13, 0, 16, 16, 16));
 	
 	
+	public static final BooleanProperty WATERLOGGED = BlockStateProperties.WATERLOGGED;
+	
 	public InkwellBlock()
 	{
 		super(Properties.create(Material.GLASS));
+		this.setDefaultState(this.stateContainer.getBaseState().with(WATERLOGGED, Boolean.valueOf(false)));
 		
 		SplatcraftBlocks.inkColoredBlocks.add(this);
+	}
+	
+	@Nullable
+	@Override
+	public BlockState getStateForPlacement(BlockItemUseContext context)
+	{
+		
+		return getDefaultState().with(WATERLOGGED, Boolean.valueOf(context.getWorld().getFluidState(context.getPos()).getFluid() == Fluids.WATER));
+	}
+	
+	protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
+		builder.add(WATERLOGGED);
+	}
+	
+	public FluidState getFluidState(BlockState state) {
+		return state.get(WATERLOGGED) ? Fluids.WATER.getStillFluidState(false) : super.getFluidState(state);
+	}
+	
+	public BlockState updatePostPlacement(BlockState stateIn, Direction facing, BlockState facingState, IWorld worldIn, BlockPos currentPos, BlockPos facingPos) {
+		if (stateIn.get(WATERLOGGED)) {
+			worldIn.getPendingFluidTicks().scheduleTick(currentPos, Fluids.WATER, Fluids.WATER.getTickRate(worldIn));
+		}
+		
+		return super.updatePostPlacement(stateIn, facing, facingState, worldIn, currentPos, facingPos);
 	}
 	
 	@Override
@@ -70,14 +106,6 @@ public class InkwellBlock extends Block implements IColoredBlock
 		if(!world.isRemote && stack.getTag() != null && world.getTileEntity(pos) instanceof InkColorTileEntity)
 			ColorUtils.setInkColor(world.getTileEntity(pos), ColorUtils.getInkColor(stack));
 		super.onBlockPlacedBy(world, pos, state, entity, stack);
-	}
-	
-	@Override
-	public ActionResultType onBlockActivated(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockRayTraceResult rayTraceResult)
-	{
-		if(world.getTileEntity(pos) instanceof InkColorTileEntity)
-			ColorUtils.setPlayerColor(player, ColorUtils.getInkColor(world.getTileEntity(pos)));
-		return ActionResultType.SUCCESS;
 	}
 	
 	@Override
