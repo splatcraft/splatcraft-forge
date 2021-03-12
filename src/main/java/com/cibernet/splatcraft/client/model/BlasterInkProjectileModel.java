d4c10c84e8b771147d69818545f3c75c7f486638
Cibernet <41699236+Cibernet83@users.noreply.github.com>
2021-03-12 13:58:36 -0500

    Improved Ink Projectile Models

---
+++ src/main/java/com/cibernet/splatcraft/client/model/BlasterInkProjectileModel.java
@@ -0,0 +1,42 @@
+package com.cibernet.splatcraft.client.model;// Made with Blockbench 3.8.0
+// Exported for Minecraft version 1.15
+// Paste this class into your mod and generate all required imports
+
+
+import com.cibernet.splatcraft.client.model.InkProjectileModel;
+import com.cibernet.splatcraft.entities.InkProjectileEntity;
+import com.mojang.blaze3d.matrix.MatrixStack;
+import com.mojang.blaze3d.vertex.IVertexBuilder;
+import net.minecraft.client.renderer.model.ModelRenderer;
+
+public class BlasterInkProjectileModel extends InkProjectileModel {
+	private final ModelRenderer main;
+
+	public BlasterInkProjectileModel()
+	{
+		textureWidth = 16;
+		textureHeight = 16;
+
+		main = new ModelRenderer(this);
+		main.setRotationPoint(0.0F, -1.0F, 0.0F);
+		main.setTextureOffset(0, 0).addBox(-2.0F, -2.0F, -2.0F, 4.0F, 4.0F, 4.0F, -0.5F, false);
+		main.setTextureOffset(0, 8).addBox(-1.5F, -1.5F, 1.25F, 3.0F, 3.0F, 1.0F, -0.2F, false);
+	}
+
+	@Override
+	public void setRotationAngles(InkProjectileEntity entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch)
+	{
+		main.rotateAngleZ = ageInTicks*0.6f;
+	}
+
+	@Override
+	public void render(MatrixStack matrixStack, IVertexBuilder buffer, int packedLight, int packedOverlay, float red, float green, float blue, float alpha){
+		main.render(matrixStack, buffer, packedLight, packedOverlay, red, green, blue, alpha);
+	}
+
+	public void setRotationAngle(ModelRenderer modelRenderer, float x, float y, float z) {
+		modelRenderer.rotateAngleX = x;
+		modelRenderer.rotateAngleY = y;
+		modelRenderer.rotateAngleZ = z;
+	}
+}
--- src/main/java/com/cibernet/splatcraft/client/renderer/InkProjectileRenderer.java
+++ src/main/java/com/cibernet/splatcraft/client/renderer/InkProjectileRenderer.java
@@ -2,23 +2,32 @@ package com.cibernet.splatcraft.client.renderer;
 
 import com.cibernet.splatcraft.Splatcraft;
 import com.cibernet.splatcraft.SplatcraftConfig;
+import com.cibernet.splatcraft.client.model.BlasterInkProjectileModel;
 import com.cibernet.splatcraft.client.model.InkProjectileModel;
+import com.cibernet.splatcraft.client.model.ShooterInkProjectileModel;
 import com.cibernet.splatcraft.entities.InkProjectileEntity;
 import com.cibernet.splatcraft.util.ColorUtils;
 import com.mojang.blaze3d.matrix.MatrixStack;
-import com.mojang.blaze3d.platform.GlStateManager;
 import net.minecraft.client.renderer.IRenderTypeBuffer;
 import net.minecraft.client.renderer.entity.EntityRenderer;
 import net.minecraft.client.renderer.entity.EntityRendererManager;
 import net.minecraft.client.renderer.entity.IEntityRenderer;
 import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.util.ResourceLocation;
-import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.vector.Vector3f;
+
+import java.util.TreeMap;
 
 public class InkProjectileRenderer extends EntityRenderer<InkProjectileEntity> implements IEntityRenderer<InkProjectileEntity, InkProjectileModel>
 {
-	private static final ResourceLocation TEXTURE = new ResourceLocation(Splatcraft.MODID, "textures/entity/ink_projectile.png");
-	private final InkProjectileModel MODEL = new InkProjectileModel();
+	private static final ResourceLocation TEXTURE = new ResourceLocation(Splatcraft.MODID, "textures/entity/shooter_projectile.png");
+	private final TreeMap<String, InkProjectileModel> MODELS = new TreeMap<String, InkProjectileModel>()
+	{{
+		put(InkProjectileEntity.Types.DEFAULT, new InkProjectileModel());
+		put(InkProjectileEntity.Types.SHOOTER, new ShooterInkProjectileModel());
+		put(InkProjectileEntity.Types.BLASTER, new BlasterInkProjectileModel());
+	}};
 	
 	public InkProjectileRenderer(EntityRendererManager manager)
 	{
@@ -30,7 +39,7 @@ public class InkProjectileRenderer extends EntityRenderer<InkProjectileEntity> i
 	{
 		if (entityIn.ticksExisted >= 3 || this.renderManager.info.getRenderViewEntity().getDistanceSq(entityIn) >= 12.25D)
 		{
-			float scale = entityIn.getProjectileSize();
+			float scale = entityIn.getProjectileSize()*2.5f;
 			int color = entityIn.getColor();
 
 			if(SplatcraftConfig.Client.getColorLock())
@@ -39,25 +48,38 @@ public class InkProjectileRenderer extends EntityRenderer<InkProjectileEntity> i
 			float r = (float) (Math.floor(color / (256 * 256)) / 255f);
 			float g = (float) ((Math.floor(color / 256) % 256) / 255f);
 			float b = (color % 256) / 255f;
-			
+
+			//0.30000001192092896D
 			matrixStackIn.push();
+			matrixStackIn.translate(0.0D, 0.4d/*0.15000000596046448D*/, 0.0D);
+			matrixStackIn.rotate(Vector3f.YP.rotationDegrees(MathHelper.lerp(partialTicks, entityIn.prevRotationYaw, entityIn.rotationYaw) - 180.0F));
+			matrixStackIn.rotate(Vector3f.XP.rotationDegrees(MathHelper.lerp(partialTicks, entityIn.prevRotationPitch, entityIn.rotationPitch)));
 			matrixStackIn.scale(scale, scale, scale);
-			getEntityModel().render(matrixStackIn, bufferIn.getBuffer(getEntityModel().getRenderType(TEXTURE)), packedLightIn, OverlayTexture.NO_OVERLAY, r, g, b, 1);
+
+			InkProjectileModel model = MODELS.getOrDefault(entityIn.getProjectileType(), MODELS.get(InkProjectileEntity.Types.DEFAULT));
+
+			model.setRotationAngles(entityIn, 0, 0, this.handleRotationFloat(entityIn, partialTicks), entityYaw, entityIn.rotationPitch);
+			model.render(matrixStackIn, bufferIn.getBuffer(model.getRenderType(getEntityTexture(entityIn))), packedLightIn, OverlayTexture.NO_OVERLAY, r, g, b, 1);
 			matrixStackIn.pop();
-			
+
 			super.render(entityIn, entityYaw, partialTicks, matrixStackIn, bufferIn, packedLightIn);
 		}
 	}
-	
+
+
+	protected float handleRotationFloat(InkProjectileEntity livingBase, float partialTicks) {
+		return (float)livingBase.ticksExisted + partialTicks;
+	}
+
 	@Override
 	public InkProjectileModel getEntityModel()
 	{
-		return MODEL;
+		return MODELS.get(InkProjectileEntity.Types.DEFAULT);
 	}
 	
 	@Override
 	public ResourceLocation getEntityTexture(InkProjectileEntity entity)
 	{
-		return TEXTURE;
+		return new ResourceLocation(Splatcraft.MODID, "textures/entity/"+entity.getProjectileType()+"_projectile.png");
 	}
 }
