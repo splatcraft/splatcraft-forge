a263dae02cf6a0d16fd18b039cf00976cacf8d79
Cibernet <41699236+Cibernet83@users.noreply.github.com>
2022-10-28 00:53:42 -0400

    Player Cooldown now prevents players from sprinting

--- src/main/java/net/splatcraft/forge/mixin/EntityMixin.java
+++ src/main/java/net/splatcraft/forge/mixin/EntityMixin.java
@@ -2,12 +2,15 @@ package net.splatcraft.forge.mixin;
 
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.splatcraft.forge.data.capabilities.playerinfo.IPlayerInfo;
 import net.splatcraft.forge.data.capabilities.playerinfo.PlayerInfoCapability;
 import net.splatcraft.forge.util.InkBlockUtils;
+import net.splatcraft.forge.util.PlayerCooldown;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Entity.class)
@@ -25,5 +28,18 @@ public class EntityMixin
 
 		if(info != null && InkBlockUtils.canSquidHide(entity) && info.isSquid())
 			cir.setReturnValue(true);
+	}
+
+	@Inject(method = "setSprinting", at = @At("HEAD"), cancellable = true)
+	public void setSprinting(boolean p_70031_1_, CallbackInfo ci)
+	{
+		if(!(((Object)this) instanceof PlayerEntity))
+			return;
+		if(p_70031_1_ && PlayerCooldown.hasPlayerCooldown(((PlayerEntity)(Object)this)))
+		{
+			ci.cancel();
+			((PlayerEntity)(Object)this).setSprinting(false);
+		}
+
 	}
 }
