6cf7369c1886650e7c6e4f1c7396d022683a767a
Cibernet <41699236+Cibernet83@users.noreply.github.com>
2023-03-29 13:59:53 -0400

    Added Sub Weapons entity tag

--- src/main/java/net/splatcraft/forge/data/SplatcraftTags.java
+++ src/main/java/net/splatcraft/forge/data/SplatcraftTags.java
@@ -95,6 +95,7 @@ public class SplatcraftTags
     public static class EntityTypes
     {
         public static final ITag<EntityType<?>> BYPASSES_SPAWN_SHIELD = createTag("bypasses_spawn_shield");
+        public static final ITag<EntityType<?>> SUB_WEAPONS = createTag("sub_weapons");
 
         private static ITag<EntityType<?>> createTag(String name)
         {
