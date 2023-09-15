08b019614a2c8ddb85c1a5be8b95f47b107127ef
Cibernet <41699236+Cibernet83@users.noreply.github.com>
2023-09-14 22:42:48 -0400

    Fixed splatcraft:squid_passthrough tag not working on certain blocks

--- src/main/java/net/splatcraft/forge/mixin/SquidPassthroughMixin.java
+++ src/main/java/net/splatcraft/forge/mixin/SquidPassthroughMixin.java
@@ -3,6 +3,7 @@ package net.splatcraft.forge.mixin;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.block.IronBarsBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.shapes.CollisionContext;
@@ -16,14 +17,13 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin({BlockBehaviour.class})
+@Mixin({BlockBehaviour.BlockStateBase.class})
 public class SquidPassthroughMixin
 {
-
-
-    @Inject(at = @At("TAIL"), method = "getCollisionShape", cancellable = true)
-    private void getCollisionShape(BlockState state, BlockGetter levelIn, BlockPos pos, CollisionContext context, CallbackInfoReturnable<VoxelShape> callback)
+    @Inject(at = @At("TAIL"), method = "getCollisionShape(Lnet/minecraft/world/level/BlockGetter;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/phys/shapes/CollisionContext;)Lnet/minecraft/world/phys/shapes/VoxelShape;", cancellable = true)
+    private void getCollisionShape(BlockGetter level, BlockPos pos, CollisionContext context, CallbackInfoReturnable<VoxelShape> callback)
     {
+        BlockBehaviour.BlockStateBase state = (BlockBehaviour.BlockStateBase) (Object) this;
         try
         {
             if(state.is(SplatcraftTags.Blocks.SQUID_PASSTHROUGH) && context instanceof EntityCollisionContext eContext &&
@@ -32,4 +32,5 @@ public class SquidPassthroughMixin
         }
         catch (IllegalStateException ignored) {}
     }
+
 }
