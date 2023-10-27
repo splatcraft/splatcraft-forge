a8a084bdaf67994c3b2def90c2a753a18e311d1d
Cibernet <41699236+Cibernet83@users.noreply.github.com>
2023-10-27 19:13:32 -0400

    ItemEntry util for item checks in codecs
    stuff like ink tank whitelists

---
+++ src/main/java/net/splatcraft/forge/util/ItemEntry.java
@@ -0,0 +1,48 @@
+package net.splatcraft.forge.util;
+
+import com.mojang.serialization.Codec;
+import net.minecraft.core.Registry;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.splatcraft.forge.items.weapons.settings.InkTankSettings;
+
+import java.util.Optional;
+
+public record ItemEntry
+		(
+				Item item,
+				TagKey<Item> tag
+		)
+{
+	public static final Codec<ItemEntry> CODEC = Codec.STRING.xmap(string -> {
+		if(string.indexOf('#') == 0)
+			return new ItemEntry(TagKey.create(Registry.ITEM_REGISTRY, new ResourceLocation(string.substring(1))));
+		Optional<Item> item = Registry.ITEM.getOptional(new ResourceLocation(string));
+
+		return item.map(ItemEntry::new).orElse(ItemEntry.NONE);
+	}, itemEntry -> {
+		if(itemEntry.tag != null)
+			return '#' + itemEntry.tag.location().toString();
+		else if(itemEntry.item != null)
+			return itemEntry.item.getRegistryName().toString();
+		return "";
+	}).stable();
+	public static final ItemEntry NONE = new ItemEntry((Item)null);
+
+
+	public ItemEntry(TagKey<Item> tag)
+	{
+		this(null, tag);
+	}
+	public ItemEntry(Item item)
+	{
+		this(item, null);
+	}
+
+	public boolean matches(ItemStack stack)
+	{
+		return tag == null ? (item != null && stack.is(item)) : stack.is(tag);
+	}
+}
