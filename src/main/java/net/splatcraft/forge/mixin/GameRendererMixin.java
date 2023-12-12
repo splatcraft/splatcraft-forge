bb21eea9bab4e159e0346d4e2e4271445de66592
Cibernet <41699236+Cibernet83@users.noreply.github.com>
2023-12-12 14:41:51 -0500

    Fixed mixin compile error

--- src/main/java/net/splatcraft/forge/mixin/GameRendererMixin.java
+++ src/main/java/net/splatcraft/forge/mixin/GameRendererMixin.java
@@ -14,20 +14,18 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(GameRenderer.class)
-public class GameRendererMixin {
-    @Shadow
-    @Final
-    private Minecraft minecraft;
+public class GameRendererMixin
+{
 
     /**
      * Disables view bobbing if configured.
      */
     @Inject(method = "bobView", at = @At("HEAD"), cancellable = true)
     private void onBobView(PoseStack matrices, float f, CallbackInfo ci) {
-        if (this.minecraft.player != null) {
-            if (PlayerInfoCapability.isSquid(this.minecraft.player)) {
+        if (Minecraft.getInstance().player != null) {
+            if (PlayerInfoCapability.isSquid(Minecraft.getInstance().player)) {
                 SplatcraftConfig.PreventBobView value = SplatcraftConfig.Client.preventBobView.get();
-                if (value.equals(SplatcraftConfig.PreventBobView.ALWAYS) || value.equals(SplatcraftConfig.PreventBobView.SUBMERGED) && PlayerInfoCapability.isSquid(this.minecraft.player) && InkBlockUtils.canSquidHide(this.minecraft.player))
+                if (value.equals(SplatcraftConfig.PreventBobView.ALWAYS) || value.equals(SplatcraftConfig.PreventBobView.SUBMERGED) && PlayerInfoCapability.isSquid(Minecraft.getInstance().player) && InkBlockUtils.canSquidHide(Minecraft.getInstance().player))
                     ci.cancel();
             }
         }
